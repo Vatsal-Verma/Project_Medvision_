2b64f21a93c44ea9b535dad29d6657e344f9a27d
Psychedelics <146416700+Vatsal-Verma@users.noreply.github.com>
2025-05-18 22:45:34 +0530

    Delete employee directory

--- employee/src/main/java/com/employeeProduct/employee/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.employeeProduct.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepo extends JpaRepository<Employee, Long>{
-    
-}
